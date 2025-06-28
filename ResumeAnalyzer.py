7aeb33fa1b83496a2b8f73028ed08f3228b39cd8
MANOJIT2943P <118467865+MANOJIT2943P@users.noreply.github.com>
2025-06-28 23:59:40 +0530

    Seperate tab section for improved resume

--- ResumeAnalyzer.py
+++ ResumeAnalyzer.py
@@ -1,19 +1,20 @@
 import streamlit as st
+from streamlit_quill import st_quill
 from langchain_community.document_loaders import PyPDFLoader
 import tempfile, os
 from langchain_together import ChatTogether
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser,JsonOutputParser
 from dotenv import load_dotenv
+from docx import Document
 
 load_dotenv()
 
 #Initialize model and parser and prompts
-model=ChatTogether(
-    model='meta-llama/Llama-3-70b-chat-hf',
-)
+
 
 parser1=StrOutputParser()
+parser2=JsonOutputParser()
 
 prompt1=PromptTemplate(
     template="Extract the following section from the given resume: skills, experience, Education \n {text}",
@@ -21,13 +22,28 @@ prompt1=PromptTemplate(
 )
 
 prompt2=PromptTemplate(
-    template="You are a career coach. Based on the given job description and resume, analyze skill gaps and suggest improvements in resume. Then return the imporved resume.\n Job description: {JD} \n Resume: {text}",
+    template="""You are a career coach. Based on the given job description and resume, analyze skill gaps and suggest improvements in resume. Then return the imporved resume.
+
+    Return your response in JSON format:
+    {{
+        "skill_gaps":Skill gaps mentions with proper text and bullet poins to make it attractive,
+        "suggestions":Improvement suggestions mentions with proper text and bullet poins to make it attractive,
+        "improved_resume": updated resume content in plain text but formatted as template style with high ATS score
+    }}
+
+    In your response , use plain text with - or numbering for bullet points. Do not use special characters like •, •, *, or emojis. Ensure valid JSON formatting with escaped characters if needed.
+    
+    \n Job description: {JD} 
+    \n Resume: {text}""",
     input_variables=['JD','text'],
 )
 
 #Function for Analyze
 def Analysis(resume,jobDescription):
     
+    model=ChatTogether(
+        model='meta-llama/Llama-3-70b-chat-hf',
+    )
 
     #use docloader to get content of resume
     if resume is not None:
@@ -45,13 +61,13 @@ def Analysis(resume,jobDescription):
     extracted=chain1.invoke({'text':docs[0].page_content})
 
     #Second call to get improvements
-    chain2=prompt2 | model | parser1 
+    chain2=prompt2 | model | parser2
     analysis=chain2.invoke({
         'JD':jobDescription,
         'text':docs[0].page_content
     })
 
-    return extracted,analysis
+    return analysis
 
 st.title("Resume Analyzer")
 
@@ -63,14 +79,18 @@ jobDescription=st.text_area(
 )
 
 if st.button("Analyze"):
-    extracted,analysis=Analysis(resume,jobDescription)
+    result=Analysis(resume,jobDescription)
 
-    tab1,tab2=st.tabs(['Extracted','Analysis'])
+    tab1,tab2=st.tabs(['Analysis and Suggestion','Sample Resume'])
 
     with tab1:
-        st.subheader('Your Resume Content')
-        st.write(extracted)
+        st.subheader('⚠️ Your Resume skill gaps as per Job Description')
+        st.write(result["skill_gaps"])
+
+        st.subheader('🤗 Suggestion to improve your Resume')
+        st.write(result["suggestions"])
     
     with tab2:
-        st.subheader('Analysis and Suggestion')
-        st.write(analysis)
+        st.subheader("Want a tailored resume? Click the button below")
+        st.write(result["improved_resume"])
+        
